692245ca42732aedbd834eeb565473f4ec263092
Paranoid <akshay_02@outlook.com>
2023-06-01 11:15:53 +0530

    comment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         setAccount(address);
-        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+        let contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
         const contract = new ethers.Contract(
           contractAddress,
@@ -47,12 +47,12 @@ const App = () => {
 
   return (
     <div className="text-center mx-auto">
-      {!modalOpen && (
+      {/* {!modalOpen && (
         <button  onClick={() => setModalOpen(true)}>
           Share
         </button>
       )}{" "}
-      {modalOpen && (<Modal setModalOpen={setModalOpen} contract={contract} />)}
+      {modalOpen && (<Modal setModalOpen={setModalOpen} contract={contract} />)} */}
 
       {!vmodalOpen && (
         <button  onClick={() => setVModalOpen(true)}>
